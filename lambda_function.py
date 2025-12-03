fda366e833503fdbc973d7cd0483111ddc40583b
divyani55 <divyani.rathore@fiftyfivetech.io>
2025-12-03 17:18:56 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello from CI/CD Pipeline Lambda!"
+        "body": "Hello from CI/CD Pipeline Lambda Function Latest!"
     }
